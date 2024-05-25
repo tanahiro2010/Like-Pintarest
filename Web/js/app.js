5f52127e2b1252f3a22ad394ae568aa618d140ef
tanahiro2010 <herentongkegu087@gmail.com>
2024-05-25 10:44:38 +0900

    Update

--- Web/js/app.js
+++ Web/js/app.js
@@ -1,6 +1,12 @@
 const content_box = document.querySelector('.grid-container');
 (() => {
     const xhr = new XMLHttpRequest();
+
+    // If this user logined, edit account to user dashboard
+    const cookie = document.cookie;
+    console.log("Cookie: {}".replace('{}', cookie));
+
+
     xhr.open('GET', '/api/get-images');
     xhr.send();
 
--- Web/js/login.js
+++ Web/js/login.js
@@ -19,7 +19,9 @@ button.addEventListener('click', () => {
         if (response == 'False') {
             swal.fire('Error', 'そのようなアカウントは登録されていません', 'error');
         } else {
-            document.cookie = JSON.stringify(response);
+            const cookie_data = `user=${encodeURIComponent(JSON.stringify(response))}`;
+            console.log(cookie_data)
+            document.cookie = cookie_data;
             // location.href = '/dashboard';
         }
     });
